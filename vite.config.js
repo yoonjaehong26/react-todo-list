a35b305235ecc750843a3fe6697979727615d98d
yoonjaehong26 <yjh0202261@gmail.com>
2026-01-23 22:45:26 +0900

    refactor:react 안정화 버전 사용

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,13 @@
-// vite.config.js 예시 (React Compiler가 활성화될 수 있는 부분)
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react({
       babel: {
-        plugins: [
-          ['babel-plugin-react-compiler', {}],
-        ],
+        plugins: ['babel-plugin-react-compiler'],
       },
-      // SWC를 사용하는 경우:
-      // swc: {
-      //   jsc: {
-      //     experimental: {
-      //       plugins: [
-      //         ['@swc/plugin-react-compiler', {}]
-      //       ]
-      //     }
-      //   }
-      // }
     }),
   ],
-});
+})
